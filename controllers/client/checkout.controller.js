dbc65083f9f4e4c328573ab03a23afadb0403923
Tu204845 <tu975482@gmail.com>
2026-02-27 10:36:51 +0700

    update code

--- controllers/client/checkout.controller.js
+++ controllers/client/checkout.controller.js
@@ -83,10 +83,27 @@ module.exports.order = async (req, res) => {
 }
 
 module.exports.success = async (req, res) => {
-    const orderId = req.params.orderId;
+    const order = await Order.findOne({
+        _id: req.params.orderId
 
+    })
+    for (const product of order.products) {
+        const productInfo = await Product.findOne({
+            _id: product.product_id
+        });
+
+        product.productInfo = productInfo;
+
+        product.priceNew = productsHelper.priceNewProduct(product)
+
+        product.totalPrice = product.priceNew * product.quantity
+
+
+    }
+
+    order.totalPrice = order.products.reduce((sum, item) => sum + item.totalPrice, 0)
     res.render("client/pages/checkout/success", {
-        pageTitle: "Đặt hàng thành công",
-
+        pageTitle: "Đặt hàng thành công",
+        order: order
     })
 }
