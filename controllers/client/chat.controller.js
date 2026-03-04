714866340732ee86b119690c3aa849c58a5e5008
Tu204845 <tu975482@gmail.com>
2026-03-04 14:17:39 +0700

    emoji-icon

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -1,3 +1,4 @@
+const { isObjectIdOrHexString } = require('mongoose');
 const Chat = require('../../models/chat.model')
 const User = require('../../models/user.model')
 
@@ -5,6 +6,8 @@ const User = require('../../models/user.model')
 
 module.exports.index = async (req, res) => {
     const userId = res.locals.user.id;
+    const fullName = res.locals.user.fullName;
+
 
     _io.once('connection', (socket) => {
         socket.on("CLIENT_SEND_MESSAGE", async (content) => {
@@ -13,6 +16,12 @@ module.exports.index = async (req, res) => {
                 content: content
             })
             await chat.save()
+
+            _io.emit("SERVER_RETURN_MESSAGE", {
+                userId: userId,
+                fullName: fullName,
+                content: content
+            })
         })
     })
 
@@ -31,6 +40,5 @@ module.exports.index = async (req, res) => {
     res.render('client/pages/chat/index', {
         pageTitle: "Chat",
         chats: chats,
-        user: res.locals.user
     })
 }
--- public/js/chat.js
+++ public/js/chat.js
@@ -1,3 +1,5 @@
+import * as Popper from "https://cdn.jsdelivr.net/npm/@popperjs/core@2/dist/esm/index.js";
+
 // CLIENT_SEND_MESSAGE
 const formSendData = document.querySelector('.chat .inner-form');
 // const socket = io();
@@ -13,3 +15,75 @@ if (formSendData) {
         }
     })
 }
+
+socket.on("SERVER_RETURN_MESSAGE", (data) => {
+    const myId = document.querySelector("[my-id]").getAttribute("my-id")
+    const body = document.querySelector(".chat .inner-body")
+
+    const div = document.createElement('div');
+    let htmlFullName = "";
+    if (myId == data.userId) {
+        div.classList.add("inner-outgoing")
+    }
+    else {
+        div.classList.add("inner-incoming")
+
+    }
+
+    div.innerHTML = `
+        ${htmlFullName}
+        <div class="inner-content">${data.content}</div>
+    `
+
+    body.appendChild(div)
+
+    body.scrollTop = body.scrollHeight;
+})
+
+const bodyChat = document.querySelector(".chat .inner-body")
+if (bodyChat) {
+    bodyChat.scrollTop = bodyChat.scrollHeight;
+}
+
+// Show icon chat
+// Show icon chat
+const buttonIcon = document.querySelector('.button-icon');
+const tooltip = document.querySelector('.tooltip');
+
+let popperInstance = null;
+
+if (buttonIcon && tooltip) {
+
+    buttonIcon.addEventListener('click', () => {
+
+        const isShown = tooltip.classList.toggle('show');
+
+        if (isShown) {
+
+            popperInstance = Popper.createPopper(buttonIcon, tooltip, {
+                placement: 'top',
+                strategy: 'fixed'
+            });
+        } else {
+
+            if (popperInstance) {
+                popperInstance.destroy();
+                popperInstance = null;
+            }
+        }
+
+    });
+
+}
+
+const emojiPicker = document.querySelector('emoji-picker')
+if (emojiPicker) {
+    const inputChat = document.querySelector(".chat .inner-form input[name='content']")
+    emojiPicker.addEventListener('emoji-click', (event) => {
+        const icon = event.detail.unicode;
+        inputChat.value = inputChat.value + icon;
+    })
+}
+
+
+// End show icon chat
